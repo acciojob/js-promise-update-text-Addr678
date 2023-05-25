f22c79e6a69260458a1e5e2573b28225a91ba4fa
Addr678 <128295657+Addr678@users.noreply.github.com>
2023-05-25 07:27:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+ const delayedGreeting = () =>
+        new Promise((resolve) => {
+          setTimeout(() => {
+            resolve('Hello, world!');
+          }, 1000);
+        });
+
+      // Update HTML element with the resolved value
+      const updateOutput = () => {
+        delayedGreeting().then((message) => {
+          document.getElementById('output').textContent = message;
+        });
+      };
+
+      // Call the update function
+      updateOutput();
